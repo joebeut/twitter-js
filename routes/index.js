1f315db698ed84e9b66840ad676d07708738686f
Joe Beutler <jtb2133@columbia.edu>
2015-03-08 05:20:02 -0400

    finished single routes

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,19 @@ router.get('/', function (req, res) {
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 });
 
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var tweets = tweetBank.find( {name: name} );
+  console.log("Tweet: " + tweets[0].id);
+  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: tweets } );
+});
+
+router.get('/users/:name/tweets/:id', function(req, res) {
+  var name = req.params.name;
+  var id = req.params.id;
+  var tweets = tweetBank.find( {name: name, id: id} );
+  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: tweets } );
+});
+
+
 module.exports = router;
